00f36fe8e64c54f9931e2d38426820ea5bbd1148
DatanoiseTV <github@ext.no-route.org>
2024-10-15 17:06:42 +0200

    Core: Change build to RP235x (was RP2040 mistakenly) and start adding PSRAM support.

--- project_config.cmake
+++ project_config.cmake
@@ -16,6 +16,16 @@ set(AUDIO_SAMPLE_BITS 32)
 set(ENABLE_FREERTOS 1)
 set(ENABLE_AUDIO_I2S 1)
 
+# Board specific peripheral settings
+set(USE_PSRAM 1)
+set(RP2350_CS_PIN 0)
+set(FLASH_LENGTH 4096000)
+set(PSRAM_LENGTH 0x800000)
+set(RAM_LENGTH 524288)
+set(EEPROM_START 272621568)
+set(FS_START 272556032)
+set(FS_END 272621568)
+
 # Board specific I/O settings
 set(PIN_I2S_DOUT 16)
 set(PIN_I2S_BCK 17)

83831475ffda6678df8995167f63d1ea61caf310
DatanoiseTV <github@ext.no-route.org>
2024-10-15 15:32:48 +0200

    Audio: Add WiP Audio Subsystem (currently broken).

---
+++ core/include/dsp_helper.h
@@ -0,0 +1,18 @@
+#pragma once
+
+static inline int32_t float_to_int32(float input) {
+    // Simple conversion from float to int32
+    // This assumes that the input float is within the range that can be represented by int32
+    // Typically, audio samples are normalized between -1.0 and 1.0, so we multiply by the max int32 value
+    int32_t output;
+
+    if (input >= 1.0f) {
+        output = INT32_MAX;
+    } else if (input <= -1.0f) {
+        output = INT32_MIN;
+    } else {
+        output = (int32_t)(input * (float)INT32_MAX);
+    }
+
+    return output;
+}
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(USE_AUDIO_I2S)
+#include "audio_subsystem.h"
+audio_buffer_pool_t *audiopool;
+#endif
+
+#define LED_DELAY_MS 500
+
+int main() {
+    #if defined(USE_AUDIO_I2S)
+    audiopool = init_audio();
+    #else
+    #warn "Audio is not enabled"
+    #endif
+
+    // set gpio2 as output
+    gpio_init(2);
+    gpio_set_dir(2, GPIO_OUT);
+
+    printf("Hello, World!\n");
+    while (1) {
+        gpio_put(2, 1);
+        sleep_ms(LED_DELAY_MS);
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
